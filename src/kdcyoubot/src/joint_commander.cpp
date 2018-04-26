c87e8ae36e142bc407d4cce07c551547608895e2
Vasu Agrawal <vasu_agrawal@yahoo.com>
2018-04-26 03:52:04 -0400

    Adding all outputs and feedback

--- src/kdcyoubot/src/joint_commander.cpp
+++ src/kdcyoubot/src/joint_commander.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include <ros/ros.h>
 
 #include "kdcyoubot/joint_vector.h"
@@ -6,23 +7,80 @@
 #include <youbot_driver/youbot/YouBotManipulator.hpp>
 #include <youbot_driver/youbot/YouBotJointParameter.hpp>
 
+// Yay globals
+youbot::YouBotBase* youbot_base = nullptr;
+youbot::YouBotManipulator* youbot_arm = nullptr;
+
+template<typename T>
+void try_set_joint_data(youbot::YouBotJoint& joint, const T& data) {
+    try {
+        joint.setData(data);
+    } catch (const std::out_of_range& e) {
+        ROS_WARN("Error while commanding joint: %s", e.what());
+    }
+}
+
 void angle_callback(const kdcyoubot::joint_vector& msg) {
+    ROS_INFO("Called angle callback");
+    for (int i = 0; i < msg.data.size(); ++i) {
+        if (msg.data[i] != std::numeric_limits<float>::infinity()) {
+            int joint = i + 1; // Joints are 1 indexed ...
+            ROS_INFO("Sending %f radians to joint %d", msg.data[i], joint);
+            youbot::JointAngleSetpoint setpoint;
+            setpoint.angle = msg.data[i] * radians;
+
+            try_set_joint_data(youbot_arm->getArmJoint(joint), setpoint);
+        }
+    }
     return;
 }
 
 void velocity_callback(const kdcyoubot::joint_vector& msg) {
+    ROS_INFO("Called velocity callback");
+    for (int i = 0; i < msg.data.size(); ++i) {
+        if (msg.data[i] != std::numeric_limits<float>::infinity()) {
+            int joint = i + 1; // Joints are 1 indexed ...
+            ROS_INFO("Sending %f radians/sec to joint %d", msg.data[i], joint);
+            youbot::JointVelocitySetpoint setpoint;
+            setpoint.angularVelocity = msg.data[i] * radians_per_second;
+
+            try_set_joint_data(youbot_arm->getArmJoint(joint), setpoint);
+        }
+    }
     return;
 }
 
 void torque_callback(const kdcyoubot::joint_vector& msg) {
+    ROS_INFO("Called torque callback");
+    for (int i = 0; i < msg.data.size(); ++i) {
+        if (msg.data[i] != std::numeric_limits<float>::infinity()) {
+            int joint = i + 1; // Joints are 1 indexed ...
+            ROS_INFO("Sending %f Nm to joint %d", msg.data[i], joint);
+            youbot::JointTorqueSetpoint setpoint;
+            setpoint.torque = msg.data[i] * newton_meters;
+
+            try_set_joint_data(youbot_arm->getArmJoint(joint), setpoint);
+        }
+    }
     return;
 }
 
 void current_callback(const kdcyoubot::joint_vector& msg) {
+    ROS_INFO("Called current callback");
+    for (int i = 0; i < msg.data.size(); ++i) {
+        if (msg.data[i] != std::numeric_limits<float>::infinity()) {
+            int joint = i + 1; // Joints are 1 indexed ...
+            ROS_INFO("Sending %f A to joint %d", msg.data[i], joint);
+            youbot::JointCurrentSetpoint setpoint;
+            setpoint.current = msg.data[i] * ampere;
+
+            try_set_joint_data(youbot_arm->getArmJoint(joint), setpoint);
+        }
+    }
     return;
 }
 
-void fold(youbot::YouBotManipulator* youbot_arm) {
+void fold() {
     const std::vector<youbot::JointAngleSetpoint> fold_angles = {
         0.11 * radian,
         0.11 * radian,
@@ -39,6 +97,39 @@ void fold(youbot::YouBotManipulator* youbot_arm) {
     SLEEP_MILLISEC(4000);
 }
 
+void send_feedback(ros::Publisher& angle_pub, ros::Publisher& velocity_pub,
+        ros::Publisher& torque_pub, ros::Publisher& current_pub) {
+
+    kdcyoubot::joint_vector angle_feedback;
+    kdcyoubot::joint_vector velocity_feedback;
+    kdcyoubot::joint_vector torque_feedback;
+    kdcyoubot::joint_vector current_feedback;
+
+    for (int i = 0; i < 5; ++i) {
+        auto& joint = youbot_arm->getArmJoint(i + 1); // Stupid 1 indexing ...
+
+        youbot::JointSensedAngle angle;
+        youbot::JointSensedVelocity velocity;
+        youbot::JointSensedTorque torque;
+        youbot::JointSensedCurrent current;
+
+        joint.getData(angle);
+        joint.getData(velocity);
+        joint.getData(torque);
+        joint.getData(current);
+
+        angle_feedback.data[i] = angle.angle.value();
+        velocity_feedback.data[i] = velocity.angularVelocity.value();
+        torque_feedback.data[i] = torque.torque.value();
+        current_feedback.data[i] = current.current.value();
+    }
+
+    angle_pub.publish(angle_feedback);
+    velocity_pub.publish(velocity_feedback);
+    torque_pub.publish(torque_feedback);
+    current_pub.publish(current_feedback);
+}
+
 /**
  * Main routine that manages the youbot arm
  *
@@ -53,7 +144,7 @@ void fold(youbot::YouBotManipulator* youbot_arm) {
  * at a time.
  */
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "joint-commander");
+    ros::init(argc, argv, "joint_commander");
     ros::NodeHandle n;
 
     // Set up subscribers for command messages
@@ -71,8 +162,8 @@ int main(int argc, char **argv) {
     // Set up youbot arm
     ROS_INFO("Loading configuration from: %s", YOUBOT_CONFIGURATIONS_DIR);
 
-    auto youbot_base = new youbot::YouBotBase("youbot-base", YOUBOT_CONFIGURATIONS_DIR);
-    auto youbot_arm = new youbot::YouBotManipulator("youbot-manipulator", YOUBOT_CONFIGURATIONS_DIR);
+    youbot_base = new youbot::YouBotBase("youbot-base", YOUBOT_CONFIGURATIONS_DIR);
+    youbot_arm = new youbot::YouBotManipulator("youbot-manipulator", YOUBOT_CONFIGURATIONS_DIR);
 
     if (youbot_base == nullptr || youbot_arm == nullptr) {
         ROS_ERROR("Failed to initialize the arm");
@@ -85,7 +176,7 @@ int main(int argc, char **argv) {
     youbot_arm->calibrateGripper();
     ROS_INFO("Calibrated arm");
 
-    fold(youbot_arm);
+    fold();
     ROS_INFO("Folded arm");
 
     // Experimentally determined joint limits from the arm:
@@ -108,20 +199,11 @@ int main(int argc, char **argv) {
 
     // Main loop
     while(ros::ok()) {
-        //@TODO: Fill these in with actual feedback
-        kdcyoubot::joint_vector angle_feedback;
-        kdcyoubot::joint_vector velocity_feedback;
-        kdcyoubot::joint_vector torque_feedback;
-        kdcyoubot::joint_vector current_feedback;
-
-
-
-        angle_pub.publish(angle_feedback);
-        velocity_pub.publish(velocity_feedback);
-        torque_pub.publish(torque_feedback);
-        current_pub.publish(current_feedback);
-
+        // Apply the control first (if any), and then send all feedback.
         ros::spinOnce();
+
+        send_feedback(angle_pub, velocity_pub, torque_pub, current_pub);
+
         loop_rate.sleep();
     }
 
